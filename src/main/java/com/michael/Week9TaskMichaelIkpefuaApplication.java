7d6deb9bbc12a1736848a7a14d79cfe4d087dc77
leafmandhunter <ikpefuamichael@gmail.com>
2021-05-24 21:48:10 +0100

    Initial Project Setup

---
+++ src/main/java/com/michael/Week9TaskMichaelIkpefuaApplication.java
@@ -0,0 +1,13 @@
+package com.michael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9TaskMichaelIkpefuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week9TaskMichaelIkpefuaApplication.class, args);
+    }
+
+}
